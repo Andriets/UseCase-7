2f85ac671d4dd2aa2fb1d47139482520cd5bbc66
KolyaNu4 <mykola.andrietc@gmail.com>
2023-07-05 13:54:45 +0300

    UC#7 Infrastructure setup

---
+++ UseCase-7/Player/Player.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UseCase_7.PlayerTask
+{
+    public class Player
+    {
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+
+        public int Experience { get; set; }
+
+        public List<int> Skills { get; set; }
+    }
+}
---
+++ UseCase-7/Student/Student.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UseCase_7.StudentTask
+{
+    public class Student
+    {
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+
+        public int Grade { get; set; }
+
+        public bool Exceptional { get; set; }
+
+        public bool HonorRoll { get; set; }
+
+        public bool Passed { get; set; }
+    }
+}
