88dd028fe2f1e4009b6a061c44fb60b95dd4c2c5
KolyaNu4 <mykola.andrietc@gmail.com>
2023-07-05 15:17:23 +0300

    UC#7 Create unit tests

---
+++ UseCase-7Tests/PlayerTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UseCase_7.PlayerTask;
+
+namespace UseCase_7Tests
+{
+    public class PlayerTests
+    {
+        private PlayerAnalyzer _playerAnalyzer;
+
+        public PlayerTests()
+        {
+            _playerAnalyzer = new PlayerAnalyzer();
+        }
+
+
+    }
+}
